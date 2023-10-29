8e26b5164a976eacb0a5fc4375b056cc08b974c6
Houssem Romdhani <romdhanihoussem77@gmail.com>
2023-10-29 16:41:37 +0100

    email validation on client side

--- Client1/Program.cs
+++ Client1/Program.cs
@@ -1,4 +1,6 @@
-﻿using ValueObjects;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using ValueObjects;
 
 namespace Client1;
 
@@ -6,16 +8,42 @@ internal class Program
 {
     static void Main()
     {
-        var customers = new List<Customer>
+        var customers = new List<Customer>();
+
+        if (IsValidEmail("scottarchibald.42@yy"))
         {
-             Customer.Create("Scott", "Archibald", "scottarchibald.42@yy"),
-             Customer.Create("Travis", "Brooks", ""),
-             Customer.Create("Victor", "Erickson", "victor.erickson@gmail.com"),
-             Customer.Create("Thomas", "Adderiy", null),
-        };
+            customers.Add(Customer.Create("Scott", "Archibald", "scottarchibald.42@yy"));
+        }
+
+        if (IsValidEmail(""))
+        {
+            customers.Add(Customer.Create("Travis", "Brooks", ""));
+        }
+
+        if (IsValidEmail("victor.erickson@gmail.com"))
+        {
+            customers.Add(Customer.Create("Victor", "Erickson", "victor.erickson@gmail.com"));
+        }
+
+        if (IsValidEmail(null))
+        {
+            customers.Add(Customer.Create("Thomas", "Adderiy", null));
+        }
 
         customers.Print();
 
         Console.ReadKey();
+    }
+
+    public static bool IsValidEmail(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return false;
+
+        Regex regex = new(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+        Match match = regex.Match(value);
+
+        return match.Success;
     }
 }
--- Client2/Program.cs
+++ Client2/Program.cs
@@ -1,4 +1,5 @@
-﻿using ValueObjects;
+﻿using System.ComponentModel.DataAnnotations;
+using ValueObjects;
 
 namespace Client2;
 
@@ -6,16 +7,37 @@ internal class Program
 {
     static void Main()
     {
-        var customers = new List<Customer>
+        var customers = new List<Customer>();
+
+        if (IsValidEmail("scottarchibald.42@yy"))
         {
-          Customer.Create("Scott", "Archibald", "scottarchibald.42@yy"),
-          Customer.Create("Travis", "Brooks", ""),
-          Customer.Create("Victor", "Erickson", "victor.erickson@gmail.com"),
-          Customer.Create("Thomas", "Adderiy", null),
-        };
+            customers.Add(Customer.Create("Scott", "Archibald", "scottarchibald.42@yy"));
+        }
+
+        if (IsValidEmail(""))
+        {
+            customers.Add(Customer.Create("Travis", "Brooks", ""));
+        }
+
+        if (IsValidEmail("victor.erickson@gmail.com"))
+        {
+            customers.Add(Customer.Create("Victor", "Erickson", "victor.erickson@gmail.com"));
+        }
+
+        if (IsValidEmail(null))
+        {
+            customers.Add(Customer.Create("Thomas", "Adderiy", null));
+        }
 
         customers.Print();
 
         Console.ReadKey();
     }
+
+
+    public static bool IsValidEmail(string value)
+    {
+        return new EmailAddressAttribute().IsValid(value);
+    }
+    
 }
